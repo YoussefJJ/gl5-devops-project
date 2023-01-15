576394e6043498649063627af675cdc41efc29a8
Youssef Jerbi <youssefjerbi@insat.u-carthage.tn>
2023-01-15 19:13:46 +0100

    change exporter

--- services/auth-service/src/util/tracer.ts
+++ services/auth-service/src/util/tracer.ts
@@ -4,19 +4,8 @@ import * as opentelemetry from "@opentelemetry/sdk-node";
 import { getNodeAutoInstrumentations } from "@opentelemetry/auto-instrumentations-node";
 import { diag, DiagConsoleLogger, DiagLogLevel } from '@opentelemetry/api';
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-grpc';
-import { JaegerExporter } from '@opentelemetry/exporter-jaeger';
 
-const options = {
-  serviceName: 'auth-service', // optional
-  // You can use the default UDPSender
-  host: process.env.JAEGER_HOST, // optional
-  port: 6832, // optional
-  // OR you can use the HTTPSender as follows
-  // endpoint: 'http://localhost:14268/api/traces',
-  maxPacketSize: 65000 // optional
-}
-
-const exporter = new JaegerExporter(options);
+const exporter = new OTLPTraceExporter();
 
 // For troubleshooting, set the log level to DiagLogLevel.DEBUG
 diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.INFO);
