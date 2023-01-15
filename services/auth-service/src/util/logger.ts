99918ec0b79b648f7e3ae1b88113c9db967cb636
Youssef Jerbi <youssefjerbi@insat.u-carthage.tn>
2023-01-15 10:29:03 +0100

    remove commented code

--- services/auth-service/src/util/logger.ts
+++ services/auth-service/src/util/logger.ts
@@ -1,11 +1,5 @@
 import { createLogger, format, transports } from 'winston'
 
-// const httpTransportOptions = {
-//   host: 'http-intake.logs.datadoghq.com',
-//   path: '/api/v2/logs?dd-api-key=fa6f5a17185a7e92e45761fcd483043074130371&ddsource=nodejs&service=auth-service',
-//   ssl: true
-// };
-
 export const logger = createLogger({
     level: 'info',
     exitOnError: false,
