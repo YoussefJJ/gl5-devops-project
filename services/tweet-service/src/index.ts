f9289084b6c28dbbfd7c8f797b81a312433d52ca
Youssef Jerbi <youssefjerbi@insat.u-carthage.tn>
2023-01-17 18:49:56 +0100

    add limiter rule

--- services/tweet-service/src/index.ts
+++ services/tweet-service/src/index.ts
@@ -14,6 +14,11 @@ const limiter = rateLimit({
 	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
 	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+    skip: (req: Request) => {
+        const { path } = req;
+        const skipPaths = ['/metrics'];
+        return skipPaths.includes(path);
+    }
 })
 
 // Apply the rate limiting middleware to all requests
