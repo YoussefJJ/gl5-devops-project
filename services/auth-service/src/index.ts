5c2e2b592ab759a65aabc51c6ab36590909584b1
Youssef Jerbi <youssefjerbi@insat.u-carthage.tn>
2023-01-17 18:31:21 +0100

    add rate limiter to both services

--- services/auth-service/src/index.ts
+++ services/auth-service/src/index.ts
@@ -3,8 +3,20 @@ import userRouter from './routes/user.routes';
 import { register } from './util/metrics'
 import cors from 'cors';
 import { requestUUIDMiddleware } from './middlewares/requestUUID';
+import rateLimit from 'express-rate-limit'
+
 
 const app: Express = express();
+
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
+
+// Apply the rate limiting middleware to all requests
+app.use(limiter)
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
--- services/tweet-service/src/index.ts
+++ services/tweet-service/src/index.ts
@@ -5,8 +5,19 @@ import retweetRouter from './routes/retweet.route';
 import tweetRouter from './routes/tweet.route';
 import { register } from './util/metrics';
 // import retweetRouter from './routes/retweet.routes';
+import rateLimit from 'express-rate-limit'
 
 const app: Express = express();
+
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
+
+// Apply the rate limiting middleware to all requests
+app.use(limiter)
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
